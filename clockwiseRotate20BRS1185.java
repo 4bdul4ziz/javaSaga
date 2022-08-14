50bbae6707b31e74d2130c2bda3d00e90b05f831
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-08-14 23:09:50 +0530

    array clockwise rotation

---
+++ clockwiseRotate20BRS1185.java
@@ -0,0 +1,18 @@
+/*
+ * Write a Java program to cyclically rotate a given array clockwise by one. 
+ */
+public class clockwiseRotate20BRS1185 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = temp;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    
+    
+}
