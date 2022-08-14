a9be928e37d714abd4298f862f3ceb727c51f4a9
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-08-14 22:17:10 +0530

    vowels and consonants in a string

---
+++ vowelConsonant.java
@@ -0,0 +1,23 @@
+/*
+ * Write a simple java program to count the number of vowels and consonants in your name and print their respective count.
+ */
+public class vowelConsonant {
+    public static void main(String[] args) {
+        String name = "Abdul Aziz";
+        int vowel = 0;
+        int consonant = 0;
+        for (int i = 0; i < name.length(); i++) {
+            if (name.charAt(i) == 'a' || name.charAt(i) == 'e' || name.charAt(i) == 'i' || name.charAt(i) == 'o' ||
+                    name.charAt(i) == 'u' || name.charAt(i) == 'A' || name.charAt(i) == 'E' || name.charAt(i) == 'I' ||
+                    name.charAt(i) == 'O' ||
+                    name.charAt(i) == 'U') {
+                vowel++;
+            } else {
+                consonant++;
+            }
+        }
+        System.out.println("The number of vowels in " + name + " is " + vowel);
+        System.out.println("The number of consonants in " + name + " is " + consonant);
+    }
+     
+}
