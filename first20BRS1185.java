d29d6914fae82e7a9e5a16ef629f82130c168470
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-08-21 02:39:25 +0530

    lab 4

---
+++ first20BRS1185.java
@@ -0,0 +1,18 @@
+
+    class First {
+        int data = 40;
+
+        void msg() {
+            System.out.println("Hello java");
+        }
+
+    }
+
+    public class first20BRS1185 {
+        public static void main(String args[]) {
+            First obj = new First();
+            System.out.println(obj.data);
+            obj.msg();
+        }
+    }
+
