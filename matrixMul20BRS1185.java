e6df3bf9ba383367d96d9d37d17a8b0edb3fc1b4
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-08-21 02:40:01 +0530

    matrix mul

---
+++ matrixMul20BRS1185.java
@@ -0,0 +1,24 @@
+/*
+ * Using a simple java program, demonstrate the working of matrix multiplication using arrays. 
+ */
+
+public class matrixMul20BRS1185 {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] c = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                c[i][j] = a[i][j] * b[i][j];
+            }
+        }
+        System.out.println("Product of the matrices is: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
