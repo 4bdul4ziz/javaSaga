b22bf7ca22aa0ccc534d27ea0cffe2edef3eb401
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-08-30 16:02:48 +0530

    prime num

---
+++ primepackage/primeNum.java
@@ -0,0 +1,15 @@
+package primepackage;
+
+public class primeNum {
+    public boolean checkForPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }   
+}
