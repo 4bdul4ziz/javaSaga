e6db27baca8ebc565372bbacb1c463a49aa7c740
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-08-30 16:01:50 +0530

    removing old stuff

--- p1/second20BRS1185.java
+++
@@ -1,9 +0,0 @@
-package p1;
-
-public class second20BRS1185 {
-    
-    public void msg() {
-        System.out.println("Hello");
-    }
-}
-
--- p1/three20BRS1185.java
+++
@@ -1,7 +0,0 @@
-package p1;
-
-public class three20BRS1185 {
-    protected void msg() {
-        System.out.println("Hello");
-    }
-}
